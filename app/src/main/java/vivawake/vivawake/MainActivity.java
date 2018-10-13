c237e2d4fab05511ce9ce960c7393cc6a7045611
blee255 <35086995+blee255@users.noreply.github.com>
2018-10-13 03:38:11 -0700

    Update app/src/main/java/vivawake/vivawake/MainActivity.java

--- app/src/main/java/vivawake/vivawake/MainActivity.java
+++ app/src/main/java/vivawake/vivawake/MainActivity.java
@@ -45,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        Toast.makeText(getApplicationContext(), "id" + id, 500).show();
-        startActivity(new Intent(this, SettingsActivity.class));
+        Toast.makeText(getApplicationContext(), "id" + id, Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, ActivityActivity.class));
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
