6bbebc81b73b24c53abf5d2fb7ff95a717fbcd6e
Jason Dai <jad054@ucsd.edu>
2018-10-13 10:51:07 -0700

    save feature

--- app/src/main/java/vivawake/vivawake/SetupActivity.java
+++ app/src/main/java/vivawake/vivawake/SetupActivity.java
@@ -7,7 +7,10 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -19,6 +22,9 @@ public class SetupActivity extends AppCompatActivity {
     AlarmManager alarmManager1;
     Calendar calendar;
     Context context;
+    Spinner ringtoneSpinner;
+    ArrayAdapter<String> ringtoneAdapter;
+    String alarmName, ringtone;
 
 
     @Override
@@ -29,7 +35,7 @@ public class SetupActivity extends AppCompatActivity {
 
         alarmManager1 = (AlarmManager) getSystemService(ALARM_SERVICE);
         timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
-
+        ringtoneSpinner = findViewById(R.id.ringtoneSpinner);
         calendar = Calendar.getInstance();
 
         //temp alarm
@@ -48,29 +54,34 @@ public class SetupActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                calendar.set(Calendar.HOUR_OF_DAY, timePicker1.getHour());
-                calendar.set(Calendar.MINUTE, timePicker1.getMinute());
+                int hour = timePicker1.getHour();
+                int minute = timePicker1.getMinute();
+                //converts edit text to string
+                EditText alarmNameEditText = (EditText) findViewById(R.id.alarmNameEditText);
+                alarmName = alarmNameEditText.getText().toString();
+     //           ringtone = ringtoneSpinner.getSelectedItem().toString();
 
+                calendar.set(Calendar.HOUR_OF_DAY, hour);
+                calendar.set(Calendar.MINUTE, minute);
 
-                Alarm newAlarm = new Alarm(calendar);
-                Toast.makeText(getApplicationContext(),"Alarm saved", Toast.LENGTH_SHORT).show();
+   //             saveAlarm(v, hour, minute, alarmName, ringtone);
 
             }
         });
         this.context = this;
-
-
     }
 
     public void saveAlarm(View view, int hour, int minute, String alarmName, String ringtoneFileName){
         SharedPreferences sharedPref = getSharedPreferences(alarmName, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-
+/*
         editor.putString("alarmName", alarmName);
         editor.putInt("hour", hour);
         editor.putInt("minute", minute);
         editor.putString("ringtone", ringtoneFileName);
         editor.apply();
+*/
+        Toast.makeText(getApplicationContext(),"Alarm saved", Toast.LENGTH_SHORT).show();
     }
 
     private void setupActionBar() {
