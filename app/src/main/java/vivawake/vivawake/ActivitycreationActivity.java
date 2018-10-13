db9560911a583bd0e34c6013b8a60cb2375408f8
blee255 <35086995+blee255@users.noreply.github.com>
2018-10-13 10:49:15 -0700

    updating activity page

--- app/src/main/java/vivawake/vivawake/ActivitycreationActivity.java
+++ app/src/main/java/vivawake/vivawake/ActivitycreationActivity.java
@@ -6,12 +6,15 @@ import android.app.TimePickerDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
 
 public class ActivitycreationActivity extends AppCompatActivity {
     EditText hours;
     EditText minutes;
     EditText name;
+    LinearLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class ActivitycreationActivity extends AppCompatActivity {
         minutes = (EditText) findViewById(R.id.MinutesTxt);
         name = (EditText) findViewById(R.id.ActivityNameTxt);
         Button save = (Button) findViewById(R.id.saveButton);
+        mLayout = (LinearLayout) findViewById(R.id.linearL);
 
         save.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -29,8 +33,16 @@ public class ActivitycreationActivity extends AppCompatActivity {
                 int minute = Integer.parseInt(minutes.getText().toString());
                 String activityName = name.getText().toString();
                 activity activity = new activity(activityName, hour, minute);
-
+                mLayout.addView(createNewButton(activity));
             }
         });
+    }
+
+    protected Button createNewButton(activity activity){
+        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final Button button = new Button(this);
+        button.setLayoutParams(lparams);
+        button.setText(activity.getHour() + " : " + activity.getMinutes() + " " + activity.getName());
+        return button;
     }
 }
