82347366fbaae9886a39bf259552671848ebf9e8
khl089 <35357693+khl089@users.noreply.github.com>
2018-10-13 01:17:13 -0700

    Added settings view

--- app/src/main/java/vivawake/vivawake/MainActivity.java
+++ app/src/main/java/vivawake/vivawake/MainActivity.java
@@ -1,5 +1,6 @@
 package vivawake.vivawake;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        Toast.makeText(getApplicationContext(), "id" + id, 500).show();
+        startActivity(new Intent(this, SettingsActivity.class));
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
