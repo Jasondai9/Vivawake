f04a8a1b272ecc30cb33287588fc1e4f5564e9f7
blee255 <35086995+blee255@users.noreply.github.com>
2018-10-13 22:20:02 -0700

    Recycler View

--- app/src/main/java/vivawake/vivawake/ActivityActivity.java
+++ app/src/main/java/vivawake/vivawake/ActivityActivity.java
@@ -1,21 +1,34 @@
 package vivawake.vivawake;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
+import java.util.ArrayList;
 
 public class ActivityActivity extends AppCompatActivity {
+    private static final String TAG = "ActivityActivity";
 
+    //vars
+    private ArrayList<String> activityNames = new ArrayList<>();
+    private ArrayList<String> hourTimes = new ArrayList<>();
+    private ArrayList<String> minuteTimes = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        initActivities();
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,8 +38,29 @@ public class ActivityActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void initActivities(){
+        SharedPreferences sharedCounter = getSharedPreferences("activityCounter", Context.MODE_PRIVATE);
+        int activityCounter = sharedCounter.getInt("counter", 0);
+        String activityName;
+        SharedPreferences pref;
+        for(int i = 1; i <= activityCounter; i++){
+            pref = getSharedPreferences("Activity"+i, Context.MODE_PRIVATE);
+            activityNames.add(pref.getString("Name", ""));
+            hourTimes.add(Integer.toString(pref.getInt("Hour", 0)));
+            minuteTimes.add(Integer.toString(pref.getInt("Minute", 0)));
+        }
+        initRecyclerView();
+    }
     public void openActivityCreation(){
        startActivity(new Intent(this, ActivitycreationActivity.class));
+    }
 
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: int recyclerview");
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter_Activity adapter = new RecyclerViewAdapter_Activity(this, activityNames, hourTimes, minuteTimes);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/vivawake/vivawake/RecyclerViewAdapter_Activity.java
@@ -0,0 +1,73 @@
+package vivawake.vivawake;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter_Activity extends RecyclerView.Adapter<RecyclerViewAdapter_Activity.ViewHolder>{
+    private static final String TAG = "RecyclerViewAdapter_Act";
+
+    private ArrayList<String> mActivityNames = new ArrayList<>();
+    private ArrayList<String> mHourTimes = new ArrayList<>();
+    private ArrayList<String> mMinuteTimes = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter_Activity(Context context, ArrayList<String> activityNames, ArrayList<String> hourTimes, ArrayList<String> minuteTimes){
+        mActivityNames = activityNames;
+        mHourTimes = hourTimes;
+        mMinuteTimes = minuteTimes;
+        mContext = context;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_layout, viewGroup, false);
+        ViewHolder holder = new ViewHolder(viewGroup);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int i) {
+        Log.d(TAG, "onBindViewHolder: called");
+        holder.activityName.setText(mActivityNames.get(i));
+        holder.hour.setText(mHourTimes.get(i));
+        holder.minutes.setText(mMinuteTimes.get(i));
+        holder.activity_layout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d(TAG, "OnClick: clicked on: " + mActivityNames.get(i));
+                Toast.makeText(mContext, mActivityNames.get(i), Toast.LENGTH_LONG).show();
+            }
+        });
+        
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mActivityNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView activityName, hour, minutes;
+        RelativeLayout activity_layout;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            activityName = itemView.findViewById(R.id.activity_name);
+            hour = itemView.findViewById(R.id.hour_text);
+            minutes = itemView.findViewById(R.id.minutes_text);
+            activity_layout = itemView.findViewById(R.id.activity_layout);
+
+        }
+    }
+}
